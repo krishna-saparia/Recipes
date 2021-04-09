887e55074181e12be0dca37e74495533ffcece77
krishna-saparia <krishnasaparia43045@gmail.com>
2021-04-08 21:55:25 -0400

    Add recipe items

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,14 +7,24 @@ import { IonicModule } from '@ionic/angular';
 import { RecipesPageRoutingModule } from './recipes-routing.module';
 
 import { RecipesPage } from './recipes.page';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipesPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    // RouterModule.forChild(routes),
     RecipesPageRoutingModule
   ],
-  declarations: [RecipesPage]
+  declarations: [RecipesPage, RecipeItemComponent]
 })
 export class RecipesPageModule {}
